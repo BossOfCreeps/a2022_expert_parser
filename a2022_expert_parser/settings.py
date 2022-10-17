acde999d2c95a23daccfa02c233910e1a44b5a08
Rachis Vsevolod <seva-ra4is@mail.ru>
2022-10-17 13:44:44 +0700

    use sqlite3

--- a2022_expert_parser/settings.py
+++ a2022_expert_parser/settings.py
@@ -77,12 +77,8 @@ WSGI_APPLICATION = 'a2022_expert_parser.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'a2022',
-        'USER': 'a2022_user',
-        'PASSWORD': 'password',
-        'HOST': 'bossofcreeps.site',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'sqlite.db',
     }
 }
 
